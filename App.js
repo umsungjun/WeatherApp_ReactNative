cbfac134c3bd2cbc4d8322e51308ee6e9bc89237
umsungjun <umseongjun@naver.com>
2024-07-28 21:21:37 +0900

    Feat: text test

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>실시간 업데이트 김민우 바보!</Text>
       <StatusBar style="auto" />
     </View>
   );
