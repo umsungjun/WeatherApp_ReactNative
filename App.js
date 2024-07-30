be5e6402c25b3489365ddbc093bdc391f552482b
umsungjun <umseongjun@naver.com>
2024-07-30 21:14:40 +0900

    Feat: flex 연습

--- App.js
+++ App.js
@@ -3,23 +3,12 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>실시간 업데이트 김민우 바보!</Text>
-      {/* StatusBar wifi, 배터리 상태를 의미 */}
-      <StatusBar style="auto" />
+    <View style={{ flex: 1 }}>
+      <View style={{ flex: 1, backgroundColor: 'tomato' }}></View>
+      <View style={{ flex: 5, backgroundColor: 'teal' }}></View>
+      <View style={{ flex: 1, backgroundColor: 'orange' }}></View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    fontSize: 28,
-    fontWeight: 800,
-  },
-});
+/* flex-direction 기본 값 column */
