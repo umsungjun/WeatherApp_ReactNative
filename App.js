d7b3f957a0f72d416b53caee4f286c02263d0a63
umsungjun <umseongjun@naver.com>
2024-07-30 21:45:56 +0900

    Feat: 도시, 날짜, 날씨 마크업

--- App.js
+++ App.js
@@ -3,12 +3,46 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={{ flex: 1 }}>
-      <View style={{ flex: 1, backgroundColor: 'tomato' }}></View>
-      <View style={{ flex: 5, backgroundColor: 'teal' }}></View>
-      <View style={{ flex: 1, backgroundColor: 'orange' }}></View>
+    <View style={styles.container}>
+      <StatusBar style="light" />
+      <View style={styles.city}>
+        <Text style={styles.cityName}>Seoul</Text>
+      </View>
+      <View style={styles.weather}>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+      </View>
     </View>
   );
 }
 
-/* flex-direction 기본 값 column */
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#3182f6',
+  },
+  city: {
+    flex: 1.25,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  cityName: {
+    fontSize: 60,
+    fontWeight: 500,
+    color: '#fff',
+  },
+  weather: {
+    flex: 3,
+  },
+  day: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  temp: {
+    marginTop: 50,
+    fontSize: 150,
+  },
+  description: { marginTop: -10, fontSize: 60 },
+});
