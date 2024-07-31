38067c086c11dfc559816ffbf14b9048ad5af40c
umsungjun <umseongjun@naver.com>
2024-07-31 22:03:06 +0900

    Feat: ScrollView 기능 연습

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Dimensions } from 'react-native';
+
+const SCREEN_WIDTH = Dimensions.get('window').width;
 
 export default function App() {
   return (
@@ -8,23 +10,44 @@ export default function App() {
       <View style={styles.city}>
         <Text style={styles.cityName}>Seoul</Text>
       </View>
-      <View style={styles.weather}>
+      <ScrollView
+        contentContainerStyle={styles.weather}
+        /* 수평 모드 */
+        horizontal
+        /* 페이지 쫀득하게 넘기기 */
+        pagingEnabled
+        /* 바닥 ScrollIndicator */
+        showsHorizontalScrollIndicator="false"
+        /* 바닥 ScrollIndicator Color 지정 Ios만 동작*/
+        // indicatorStyle="white"
+      >
         <View style={styles.day}>
           <Text style={styles.temp}>27</Text>
           <Text style={styles.description}>Sunny</Text>
         </View>
-      </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+      </ScrollView>
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#3182f6',
   },
   city: {
-    flex: 1.25,
+    flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
   },
@@ -33,15 +56,12 @@ const styles = StyleSheet.create({
     fontWeight: 500,
     color: '#fff',
   },
-  weather: {
-    flex: 3,
-  },
+  weather: {},
   day: {
-    flex: 1,
+    width: SCREEN_WIDTH,
     alignItems: 'center',
   },
   temp: {
-    marginTop: 50,
     fontSize: 150,
   },
   description: { marginTop: -10, fontSize: 60 },
