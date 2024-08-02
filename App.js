5d11e5b7b87ed41435ec29cfdc800c764913664c
umsungjun <umseongjun@naver.com>
2024-08-02 21:09:28 +0900

    Feat: 현재 위치정보를 가져와 도시 정보 보여주도록 구현
    - getCurrentPositionAsync({ accuracy: 5 })를 통해 현재 위치 정보(위도, 경도) 가져오도록 구현
    - Location.reverseGeocodeAsync() 인자로 위도, 경도를 전달해서 현재 도시 정보 가져오도로 구현

--- App.js
+++ App.js
@@ -1,14 +1,48 @@
+import { useEffect, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, ScrollView, Dimensions } from 'react-native';
+
+import * as Location from 'expo-location';
 
 const SCREEN_WIDTH = Dimensions.get('window').width;
 
 export default function App() {
+  const [city, setCity] = useState('...Loading');
+  const [location, setLocation] = useState(null);
+  const [ok, setOk] = useState(true);
+
+  const ask = async () => {
+    const { granted } = await Location.requestForegroundPermissionsAsync();
+    if (!granted) {
+      setOk(false);
+    }
+
+    /* 5 가능한 가장 높은 수준의 정확도. getCurrentPositionAsync() 위도 경도 구해오는 메서드 */
+    const {
+      coords: { latitude, longitude },
+    } = await Location.getCurrentPositionAsync({ accuracy: 5 });
+
+    /* reverseGeocodeAsync() 위도, 경도를 통해 현재 위치 정보를 가져오는 메서드 */
+    const location = await Location.reverseGeocodeAsync(
+      {
+        latitude,
+        longitude,
+      },
+      { useGoogleMaps: false }
+    );
+
+    setCity(location[0].city);
+  };
+
+  useEffect(() => {
+    ask();
+  }, []);
+
   return (
     <View style={styles.container}>
       <StatusBar style="light" />
       <View style={styles.city}>
-        <Text style={styles.cityName}>Seoul</Text>
+        <Text style={styles.cityName}>{city}</Text>
       </View>
       <ScrollView
         contentContainerStyle={styles.weather}
